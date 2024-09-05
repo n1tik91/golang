ea29590a5ab096ca84998bc8389ec80b957c0d92
Nitik Adhikari <n1tikk007@gmail.com>
2024-09-06 01:34:55 +0545

    ,

---
+++ LearningGO/math.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Two number's 5 and 1 are given")
+	fmt.Println("Addition      ","=",5+1)
+	fmt.Println("Subtraction   ","=",5-1)
+	fmt.Println("Multiplication","=",5*1)
+
+}
