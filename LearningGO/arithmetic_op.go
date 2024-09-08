b79e28a07f807059f28dfc6a2bbcdbcfdc3f1b73
Nitik Adhikari <n1tikk007@gmail.com>
2024-09-08 23:26:26 +0545

    ,

---
+++ LearningGO/arithmetic_op.go
@@ -0,0 +1,21 @@
+// arithmetic operations 
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int16 = 170
+	var y int16 = 83
+	//for Addition
+	fmt.Printf(" addition : %d + %d = %d\n ", x, y, x+y)
+	//for Subtraction
+	fmt.Printf("subtraction : %d - %d = %d\n", x, y, x-y)
+	//for Multiplication
+	fmt.Printf(" multiplication : %d * %d = %d\n", x, y, x*y)
+	//for Division
+	fmt.Printf(" division : %d / %d = %d\n", x, y, x/y)
+	//for Modulus
+	fmt.Printf(" remainder : %d %% %d = %d\n", x, y, x%y)
+}
