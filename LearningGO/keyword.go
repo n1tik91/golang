8e1b28dddc788b7f75cd35a81f0e0c6de481b57d
Nitik Adhikari <160871019+n1tik91@users.noreply.github.com>
2024-09-08 23:19:16 +0545

    .

--- LearningGO/keyword.go
+++ LearningGO/keyword.go
@@ -3,15 +3,13 @@ package main
 
 import "fmt"
 
-// Here, package, import, func & var are keywords 
+//  package, import, func & var are keywords 
 func main() { 
 
 // Here, a is a valid identifier 
 var a = "GeeksforGeeks"
 
 fmt.Println(a) 
-
-// Here, the default is an illegal identifier and compiler will throw an error var default = "GFG" 
 
 
 // Here, var keyword is used  to create variables Pname, Lname, and Cname are identifiers 
